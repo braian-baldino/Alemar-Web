f9ef92730308181664d08ea64b0969ae6ed14dc6
Braian Baldino <baldino.bab@hotmail.com>
2021-08-30 21:24:13 -0300

    improve login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,32 +1,23 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { useOktaAuth } from '@okta/okta-react';
 import Home from '../Home/Home';
 
 const Login = () => {
   const { authState, oktaAuth } = useOktaAuth();
-  const [userInfo, setUserInfo] = useState(null);
-
-  useEffect(() => {
-    if (!authState || !authState.isAuthenticated) {
-      // When user isn't authenticated, forget any user info
-      setUserInfo(null);
-    } else {
-      oktaAuth.getUser().then(info => {
-        setUserInfo(info);
-      });
-    }
-  }, [authState, oktaAuth]); // Update if authState changes
 
   const login = async () => {
     oktaAuth.signInWithRedirect({ originalUri: '/' });
   };
 
-  let content =
-    authState && authState.isAuthenticated ? (
-      <Home />
-    ) : (
-      <div onClick={login()}></div>
-    );
-  return <React.Fragment>{content}</React.Fragment>;
+  return (
+    <React.Fragment>
+      {authState && authState.isAuthenticated ? (
+        <Home />
+      ) : (
+        <div onClick={login()}></div>
+      )}
+    </React.Fragment>
+  );
 };
+
 export default Login;
