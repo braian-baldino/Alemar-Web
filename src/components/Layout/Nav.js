81a401fccbc230d713dcd369aa1edb90cb8b8f13
Braian Baldino <baldino.bab@hotmail.com>
2021-09-01 18:26:40 -0300

    navBar auth cleanup

--- src/components/Layout/Nav.js
+++ src/components/Layout/Nav.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import { useOktaAuth } from '@okta/okta-react';
-import MenuAppBar from '../UI/NavBar';
+import NavBar from '../UI/NavBar';
 
 const Nav = () => {
   const { authState, oktaAuth } = useOktaAuth();
 
-  const navLinks = authState.isAuthenticated
-    ? [
-        { path: '/home', text: 'Home' },
-        { path: '/clientes', text: 'Clientes' },
-        { path: '/ventas', text: 'Ordenes' },
-        { path: '/productos', text: 'Productos' },
-      ]
-    : [{ path: '/', text: 'Login' }];
+  const navLinks =
+    authState && authState.isAuthenticated
+      ? [
+          { path: '/home', text: 'Home' },
+          { path: '/clientes', text: 'Clientes' },
+          { path: '/ventas', text: 'Ordenes' },
+          { path: '/productos', text: 'Productos' },
+        ]
+      : [{ path: '/', text: 'Login' }];
 
   return (
     <nav>
-      <MenuAppBar links={navLinks} />
+      <NavBar links={navLinks} />
     </nav>
   );
 };
--- src/dummy/NavLinks.js
+++
@@ -1,9 +0,0 @@
-const NavLinks = [
-  { path: '/', text: 'Home' },
-  { path: '/clientes', text: 'Clientes' },
-  { path: '/productos', text: 'Productos' },
-  { path: '/ventas', text: 'Ventas' },
-  { path: '/estadisticas', text: 'Estadisticas' },
-];
-
-export default NavLinks;
