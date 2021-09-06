28f86f66b051b41cc05211ff771d4ca09c77d768
Braian Baldino <baldino.bab@hotmail.com>
2021-09-06 20:49:29 -0300

    color fix 2

--- src/components/UI/Navigation/NavMenu.js
+++ src/components/UI/Navigation/NavMenu.js
@@ -4,7 +4,7 @@ import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import IconButton from '@material-ui/core/IconButton';
 import DehazeIcon from '@material-ui/icons/Dehaze';
-import colors from './../../../utilities/colors.module.scss';
+import styles from './NavMenu.module.scss';
 
 export default function NavMenu(props) {
   const [anchorEl, setAnchorEl] = React.useState(null);
@@ -25,10 +25,7 @@ export default function NavMenu(props) {
         onClick={handleClick}
       >
         <IconButton
-          style={{
-            backgroundColor: colors.white,
-            color: colors.primary,
-          }}
+          className={styles.IconButton}
           aria-label='account of current user'
           aria-controls='menu-appbar'
           aria-haspopup='true'
