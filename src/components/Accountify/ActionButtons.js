1dfa0aab3282154fe62b00f5aa17bf031f82a40d
Braian Baldino <baldino.bab@hotmail.com>
2021-10-06 21:45:33 -0300

    fix for actionButtons

--- src/components/Accountify/ActionButtons.js
+++ src/components/Accountify/ActionButtons.js
@@ -2,11 +2,11 @@ import React from 'react';
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
 import RemoveIcon from '@material-ui/icons/Remove';
-import { MuiThemeProvider, createTheme } from '@material-ui/core/styles';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import colors from '../../utilities/colors.module.scss';
 
 const ActionButtons = () => {
-  const theme = createTheme({
+  const theme = createMuiTheme({
     palette: {
       primary: { main: colors.green },
       secondary: { main: colors.red },
